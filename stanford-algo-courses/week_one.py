6c0d99f5cd5fe4c9b60653c9606861848feca921
Iulia Avram <iuliaavram@Yunos-MacBook-Pro.local>
2019-08-23 07:31:38 +0100

    Finish week 1 assignment

--- stanford-algo-courses/week_one.py
+++ stanford-algo-courses/week_one.py
@@ -8,21 +8,30 @@ def digits(x):
     else:
         return int(math.log10(-n)) + 2
 
-def karatsuba(a, b):
+def karatsuba(a, b, d):
     """
     Some comments here
     """
-    if digits(a) == 1 and digits(b) == 1:
+    if d == 2:
         return a * b
+    d = d // 2
 
-    a_1 = a // (10 ** digits(a))
-    a_2 = a % (10 ** digits(a))
+    a_1 = a // (10 ** d)
+    a_2 = a % (10 ** d)
 
-    b_1 = b // (10 ** digits(b))
-    b_2 = b % (10 ** digits(b))
+    b_1 = b // (10 ** d)
+    b_2 = b % (10 ** d)
 
+    x = karatsuba(a_1, b_1, d)
+    y = karatsuba(a_2, b_2, d)
+    z = karatsuba(a_1 + a_2, b_1 + b_2, d) - x - y
+    return x * (10**(d*2)) + z * (10 ** d) + y
 
 
 if __name__ == "__main__":
     first_number, second_number = [int(x) for x in input().split(' ')]
-    print(karatsuba(first_number, second_number))
+    if digits(first_number) != digits(second_number):
+        print("We are not supporting numbers of different lengths at the moment")
+    else:
+        print(karatsuba(first_number, second_number, digits(first_number)))
+
